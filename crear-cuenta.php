7833a04cd1100bf64f22a0daed13fcc0f996cd4d
jefrey <jefreysanchez422gmail.com>
2024-10-27 23:50:01 -0500

    actualizacion

--- crear-cuenta.php
+++ crear-cuenta.php
@@ -72,7 +72,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <body>
     <main class="login-container">
-        <img src="img/logo.png" alt="Venneta">
+        <img src="img/logo1.png" alt="Venneta">
         <?php if ($error): ?>
             <p class="error"><?php echo $error; ?></p>
         <?php endif; ?>
