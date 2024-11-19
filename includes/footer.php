af68f67e5c241b937e7b4416b41c44703994caae
jefrey <jefreysanchez422gmail.com>
2024-11-19 17:08:28 -0500

    enlace corregido

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
     <img src="./img/venneta_logo.png" alt="LOGO">
     <section id="contacto" class="contacto">
         <div class="redes-sociales">
-            <a href="https://wa.me/+5731?text=Hola%2C,%20estoy%20interesado%20en%20tu%20producto" target="_blank"
+            <a href="https://wa.me/+573046165621?text=Hola%2C%20estoy%20interesado%20en%20tu%20producto" target="_blank"
                 rel="noreferrer">WhatsApp</a>
             <a href="https://www.instagram.com/venneta_col/" target="_blank" rel="noreferrer">Instagram</a>
         </div>
